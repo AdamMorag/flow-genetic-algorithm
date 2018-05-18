a8a91953aad12bd9ca603a893a2f0ec020b6496b
AdamMorag <moragadam@gmail.com>
2018-05-18 21:09:40 +0300

    Added microservice4net

---
+++ flow-genetic-algorithm/flow-genetic-algorithm/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace flow_genetic_algorithm.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        [Route("Example")]
+        public string GetExample()
+        {
+            return "Example";
+        }
+    }
+}
--- flow-genetic-algorithm/flow-genetic-algorithm/Program.cs
+++ flow-genetic-algorithm/flow-genetic-algorithm/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicroService4Net;
 
 namespace flow_genetic_algorithm
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            var microService = new MicroService();
+            microService.Run(args);
         }
     }
 }
